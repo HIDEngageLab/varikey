3ee3362c6124ea2822754fb24c6ea1481776a9b7
Roman Koch <roman.koch@gossenmetrawatt.com>
2023-08-31 13:50:27 +0200

    fixed action

--- build/update-fw.sh
+++ build/update-fw.sh
@@ -3,4 +3,4 @@
 # Copyright (c) 2023, Roman Koch, koch.roman@gmail.com
 # SPDX-License-Identifier: MIT
 
-openocd -f interface/raspberrypi-swd.cfg -f target/rp2040.cfg -c "program bin/composed_hid_device.elf verify reset exit"
+openocd -f interface/raspberrypi-swd.cfg -f target/rp2040.cfg -c "program bin/varikey.elf verify reset exit"
