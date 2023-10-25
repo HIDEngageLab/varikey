8f24d528f714a37fd8df891cf425d09c85b0875c
Roman Koch <roman.koch@gossenmetrawatt.com>
2023-10-25 16:44:25 +0200

    changed target name
    added hardware and firmware identifier to the binary name
    changed build call over build.sh

--- build.sh
+++ build.sh
@@ -6,9 +6,17 @@
 project_path=`pwd`
 
 # define hardware paltform with
-# available platforms are 
-#hw_plarform=VARIKEY_1_0
-hw_plarform=GOSSENMETRAWATT_1_0
+# available platforms are...
+hw_plarform=VARIKEY
+#hw_plarform=GOSSENMETRAWATT
 
-cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_C_COMPILER:FILEPATH=/usr/bin/arm-none-eabi-gcc -DCMAKE_CXX_COMPILER:FILEPATH=/usr/bin/arm-none-eabi-g++ -DHARDWARE_PLATFORM:STRING=${hw_plarform} -S${project_path} -B${project_path}/bin -G "Unix Makefiles"
+# define product variant
+# available products are...
+#hw_plarform=VARIKEY
+#fw_product=KEYPAD_10BWDB
+fw_product=KEYPAD_DEMO
+
+cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_C_COMPILER:FILEPATH=/usr/bin/arm-none-eabi-gcc -DCMAKE_CXX_COMPILER:FILEPATH=/usr/bin/arm-none-eabi-g++ -DHARDWARE_PLATFORM:STRING=${hw_plarform} -DFIRMWARE_PRODUCT:STRING=${fw_product} -S${project_path} -B${project_path}/bin -G "Unix Makefiles"
 cmake --build ${project_path}/bin --config Debug --target all -j 6 --
+
+
