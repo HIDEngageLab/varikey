ac7d9af1342b74802d3915d512b7b2a4863db530
Roman Koch <roman.koch@gossenmetrawatt.com>
2024-02-16 11:42:18 +0100

    fixed gossenmetrawatt i2c address and speed

--- src/platform/hardware/keymatrix/keymatrix_adp5585_gossenmetrawatt.hpp
+++ src/platform/hardware/keymatrix/keymatrix_adp5585_gossenmetrawatt.hpp
@@ -17,12 +17,12 @@ namespace platform
     {
         struct KeymatrixApd5585Gossenmetrawatt : public KeymatrixApd5585
         {
-            static const unsigned int AD5585_I2C_ADDRESS = 0x30;
+            static const unsigned int AD5585_I2C_ADDRESS = 0x34;
             static const unsigned int PICO_I2C_SDA = 12;
             static const unsigned int PICO_I2C_SCL = 13;
             static const unsigned int AD5585_RST = 14;
             static const unsigned int AD5585_INT = 11;
-            static const unsigned int PICO_I2C_SPEED = (100 * 1000);
+            static const unsigned int PICO_I2C_SPEED = (400 * 1000);
 
             static const unsigned int W = 128;
             static const unsigned int H = 32;
