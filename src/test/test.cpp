293381c411d3d161bf72fadf390976ad98170503
Koch, Roman <koch.roman@googlemail.com>
2024-06-26 08:07:56 +0200

    added set test identifier in memory

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -22,11 +22,30 @@ struct test_parameter_struct
     uint16_t identifier;
 } __attribute__((packed));
 
-test_parameter_struct * test_parameter = reinterpret_cast<test_parameter_struct *>(TEST_PARAMETER_FLASH_START);
+test_parameter_struct *test_parameter = reinterpret_cast<test_parameter_struct *>(TEST_PARAMETER_FLASH_START);
+
+static uint16_t test_group __attribute__((section(".utest_custom_section"), used));
+static uint16_t test_identifier __attribute__((section(".utest_custom_section"), used));
+//static uint16_t test_group __attribute__((section(".uninitialized_data"), used));
+//static uint16_t test_identifier __attribute__((section(".uninitialized_data"), used));
+extern char __utest_custom_section_start;
+extern char __utest_custom_section_end;
 
 int main(void)
 {
+    //test_group = 0xffff;
+    //test_identifier = 0xffff;
+
     stdio_init_all();
+
+    printf("test_group:      %04x\n", test_group);
+    printf("test_identifier: %04x\n", test_identifier);
+
+    printf("Start of .utest_custom_section: %p\n", (void *)&__utest_custom_section_start);
+    printf("End of .utest_custom_section: %p\n", (void *)&__utest_custom_section_end);
+
+    while (test_identifier == 0xffff)
+        ;
 
     printf("test group:      %d\n", test_parameter->group);
     printf("test identifier: %d\n", test_parameter->identifier);
